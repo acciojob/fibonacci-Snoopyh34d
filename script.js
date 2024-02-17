d7c3faa596b0f90af5f7c8941e6e77ecd91ce249
Prabhat Ayush <60286121+Snoopyh34d@users.noreply.github.com>
2024-02-17 10:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// function fibonacci(num) {
-// // your code here
-// }
+// // function fibonacci(num) {
+// // // your code here
+// // }
 
-// module.exports = fibonacci;
-function calculateFibonacci() {
-            var numTerms = document.getElementById("numTerms").value;
-            var sequence = [];
-            for (var i = 0; i < numTerms; i++) {
-                if (i <= 1) {
-                    sequence.push(i);
-                } else {
-                    sequence.push(sequence[i - 1] + sequence[i - 2]);
-                }
-            }
-            document.getElementById("fibonacciSequence").innerText = "Fibonacci sequence: " + sequence.join(", ");
-        }
+// // module.exports = fibonacci;
+// function calculateFibonacci() {
+//             var numTerms = document.getElementById("numTerms").value;
+//             var sequence = [];
+//             for (var i = 0; i < numTerms; i++) {
+//                 if (i <= 1) {
+//                     sequence.push(i);
+//                 } else {
+//                     sequence.push(sequence[i - 1] + sequence[i - 2]);
+//                 }
+//             }
+//             document.getElementById("fibonacciSequence").innerText = "Fibonacci sequence: " + sequence.join(", ");
+//         }
